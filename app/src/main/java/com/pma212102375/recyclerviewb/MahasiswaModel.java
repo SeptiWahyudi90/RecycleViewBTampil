61cd4a4ce769bd14628c5620a12229f1ad2283ff
SeptiWahyudi90 <septiwahyudi90@gmail.com>
2024-06-05 15:02:13 +0700

    Initial commit

---
+++ app/src/main/java/com/pma212102375/recyclerviewb/MahasiswaModel.java
@@ -0,0 +1,25 @@
+package com.pma212102375.recyclerviewb;
+
+public class MahasiswaModel
+{
+    private String NIM, Nama, JenisKelamin, JP;
+
+    public MahasiswaModel() {
+    }
+
+    public String getNIM() { return NIM; }
+
+    public void setNIM(String NIM) {this.NIM = NIM; }
+
+    public String getNama() { return Nama; }
+
+    public void setNama(String Nama) {this.Nama = Nama; }
+
+    public String getJenisKelamin() { return JenisKelamin; }
+
+    public void setJenisKelamin(String JenisKelamin) {this.JenisKelamin = JenisKelamin; }
+
+    public String getJP() { return JP; }
+
+    public void setJP(String JP) { this.JP = JP; }
+}
